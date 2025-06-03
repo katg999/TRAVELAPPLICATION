0f2c5104daf6704c911b51a0d911040c35059447
braingilbert <briangilbert@DESKTOP-BOD1Q8N>
2025-06-03 20:44:00 +0300

    CorrectedESLintErrors

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import { useState } from 'react';
-import Logo from './Logo';
-import Form from './Form';
-import PackingList from './PackingList';
-import Stats from './Stats';
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
 
-const initialItems = [
-	{ id: 1, description: 'Passports', quantity: 2, packed: false },
-	{ id: 2, description: 'Socks', quantity: 12, packed: true },
-	{ id: 3, description: 'Charger', quantity: 1, packed: false },
-];
+//const initialItems = [
+//	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+//	{ id: 2, description: 'Socks', quantity: 12, packed: true },
+//	{ id: 3, description: 'Charger', quantity: 1, packed: false },
+//];
 
 export default function App() {
-	const [items, setItems] = useState([]);
-	const numItems = items.length;
-	function handleAddItems(item) {
-		setItems((items) => [...items, item]);
-	}
+  const [items, setItems] = useState([]);
+  //const numItems = items.length;
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
-	function handleDeleteItem(id) {
-		setItems((items) => items.filter((item) => item.id !== id));
-	}
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
 
-	function handleToggleItem(id) {
-		setItems((items) =>
-			items.map((item) =>
-				item.id === id ? { ...item, packed: !item.packed } : item
-			)
-		);
-	}
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
 
-	function handleClearList() {
-		const confirmed = window.confirm(
-			'Are you sure you want to delet all items'
-		);
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delet all items"
+    );
 
-		if (confirmed) setItems([]);
-	}
+    if (confirmed) setItems([]);
+  }
 
-	return (
-		<div className='app'>
-			<Logo />
-			<Form onAddItems={handleAddItems} />
-			<PackingList
-				items={items}
-				onDeleteItem={handleDeleteItem}
-				onToggleItem={handleToggleItem}
-				onClearList={handleClearList}
-			/>
-			<Stats items={items} />
-		</div>
-	);
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
 }
